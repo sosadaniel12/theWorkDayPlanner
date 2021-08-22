b02bbe88cedd35936481eb1397ac4c83e1061c9e
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-08-22 16:11:13 -0500

    adding all intial files

---
+++ planner.js
@@ -0,0 +1,13 @@
+// when the planner opens the current day and time will be displayed at the top of the page
+
+//when the user scrolls down event block will appear from a 8 hour shift
+
+// if an event block has already passed the current time it will change the block to a red color\
+
+// if an event block is in the range of the current time it will be a neutral color
+
+// if an event block is out of the range of the current time in the future it will turn into a light blue color
+
+//when the input of an event block is saved it will then be saved into the local storage
+
+//when and if the page refreshed the event block will still have the same text inputed unless it is resetted
